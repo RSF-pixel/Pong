b4ff1841477e6b77d2d73dc970396eae81148c85
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-03-20 08:50:40 +0000

    Initial commit

---
+++ Assets/Scrpts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] float velocity = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Search the component rigidbody attached to the ball
+        //Random if goes for the right or left 
+        //if(Random.value < 0.5)
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = velocity * Vector2.right;
+        //}
+        //else
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = velocity * Vector2.left;
+        //}
+
+
+        //Get a random vector from a circle
+        GetComponent<Rigidbody2D>().velocity = velocity * Random.insideUnitCircle;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scrpts/Paddle.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+
+    [SerializeField] float velocity = 0.5f;
+
+    [SerializeField] KeyCode upKey = KeyCode.UpArrow;
+    [SerializeField] KeyCode downKey = KeyCode.DownArrow;
+
+    void Update()
+    {
+
+        //MOVE
+        //UP ARROW
+        if (Input.GetKey(upKey))
+        {
+            //Goes Up
+            //Delta time -time from the last update
+            transform.position += velocity * Vector3.up * Time.deltaTime;
+
+
+
+        }
+        //DOWN ARROW
+        else if (Input.GetKey(downKey))
+        {
+            //Goes Down
+            transform.position += velocity * Vector3.down * Time.deltaTime;
+
+
+        }
+
+        //VERIFY LIMITS
+
+        //Principal Camera Height
+        float cameraHeight = Camera.main.orthographicSize;
+
+        if(transform.position.y >= cameraHeight - 0.5f)
+        {
+            //You cant edit the component transform for performance issues so you create another variable
+            Vector3 positionAux = transform.position;
+            positionAux.y = cameraHeight - 0.5f;
+            transform.position = positionAux;
+
+        }
+        if(transform.position.y <= -cameraHeight + 0.5f)
+        {
+            //You cant edit the component transform for performance issues so you create another variable
+            Vector3 positionAux = transform.position;
+            positionAux.y = -cameraHeight + 0.5f;
+            transform.position = positionAux;
+
+        }
+
+
+       // Compact way of making the limits (Intervals)
+       //0.5f paddle height
+
+       // Vector3 positionAux = transform.position;
+       // positionAux.y = Mathf.Clamp(positionAux.y, -cameraHeight + 0.5f, cameraHeight - 0.5f);
+       // transform.position = positionAux;
+
+
+    }
+}
